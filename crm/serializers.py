dd05020697fd02ca9899b22347285ae75bd6fcc1
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2022-01-21 17:32:52 -0700

    Update serializers.py

--- crm/serializers.py
+++ crm/serializers.py
@@ -2,13 +2,15 @@ from crm.models import Navbar, NavbarItem
 from rest_framework import serializers
 
 
-class NavbarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Navbar 
-        fields = '__all__'
-
-
 class NavbarItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = NavbarItem
         fields = '__all__'
+
+
+class NavbarSerializer(serializers.ModelSerializer):
+    items = NavbarItemSerializer(read_only=True, many=True)
+    class Meta:
+        model = Navbar 
+        fields = '__all__'
+
